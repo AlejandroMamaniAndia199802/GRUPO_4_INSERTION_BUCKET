23a84be95e268d589961fff3de4c9a8b1522563a
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-09-01 11:48:28 -0500

    Update SORTER.cpp

--- SORTER.cpp
+++ SORTER.cpp
@@ -1,7 +1,7 @@
 #include "SORTER.h"
 #include <iostream>
-
-
+#include <cmath>
+#include <vector>
 
 
 void SORTER::insertionSort(int arr[], int n)
@@ -22,6 +22,40 @@ void SORTER::insertionSort(int arr[], int n)
 	}
 
 }
+
+void SORTER::bucketSort(int *arr, int n){
+    int max = 0;
+    for(int i=0; i<n; i++){
+        k = *(arr+1);
+        if(max < k){
+            max = k;
+        }
+    }
+
+    const int buckets = 10;
+
+    vector<float> b[buckets];
+
+    int devider = ceil((max +1)/buckets);
+
+    for (int i=0; i<n; i++)
+    {
+        k = *(arr+1);
+        int bi = floor(k / devider);
+        b[bi].push_back(k);
+    }
+
+    //Insertion sort in each bucket
+    for (int i =0; i<buckets; i++){
+        sort(b[i].begin(), b[i].end());
+    }
+
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < b[i].size(); j++)
+            arr[index++] = b[i][j];
+}
+
 void SORTER::insertionSort(std::vector<int> &arr)
 {
 	int size = arr.size();
