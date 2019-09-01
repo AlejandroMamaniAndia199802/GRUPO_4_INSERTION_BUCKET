044e8eab54fc8337d9faa742004611bb1bb89557
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-09-01 14:57:17 -0500

    Improve the test-main

--- test-sorter.cpp
+++ test-sorter.cpp
@@ -4,12 +4,27 @@
 
 #include "sorter.h"
 
-TEST_CASE("sort a vector")
+TEST_CASE("SORT A SET OF NUMBERS BY INSERTION USING VECTOR")
 {
-	sorter sortingTester;
-	std::vector<int> v{4, 15, 5, 6, 42, 23};
 
-	sortingTester.insertionSortVect(v);
-	CHECK(v.front() == 4);
-	CHECK(v.back() == 42);
+	sorter sortingTester;
+	std::vector<int> X{4, 15, 5, 6, 42, 23};
+	std::vector<int> sortedX{4, 5, 6, 15, 23, 42};
+	sortingTester.insertionSortVect(X);
+    for (int i = 0; i < X.size(); ++i)
+    {
+        REQUIRE(sortedX[i] == X[i]);
+    }
+}
+TEST_CASE("SORT A SET OF NUMBERS BY INSERTION USING ARRAY")
+{
+    sorter sortingTester_1;
+    int N = 6;
+    int arr[]={4, 15, 5, 6, 42, 23};
+    int sortedarr[]={4, 5, 6, 15, 23, 42};
+    sortingTester_1.insertionSortarr(arr, N);
+    for (int i = 0; i < N; ++i)
+    {
+        REQUIRE(sortedarr[i] == arr[i]);
+    }
 }
