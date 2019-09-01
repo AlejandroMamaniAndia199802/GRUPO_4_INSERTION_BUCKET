1888bb234ef5b4100db7678aba07cf0d5599254e
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-09-01 11:39:01 -0500

    Update BUCKET.h

--- BUCKET.h
+++ BUCKET.h
@@ -1,13 +1,12 @@
-//
-// Created by albin on 29/08/2019.
-//
-
 #ifndef RADIX_SORT_GRUPO_4_BUCKET_H
 #define RADIX_SORT_GRUPO_4_BUCKET_H
 
 
 class BUCKET {
-
+    int k;
+    BUCKET();
+    void sortingArray(int *arr, int n);
+    void printArray(int *arr, int n);
 };
 
 
