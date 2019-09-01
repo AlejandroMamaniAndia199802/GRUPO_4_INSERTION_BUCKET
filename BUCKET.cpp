ff602a97eccc0e69eee574806f28c336308d269d
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-09-01 11:37:42 -0500

    Update BUCKET.cpp

--- BUCKET.cpp
+++ BUCKET.cpp
@@ -1,5 +1,50 @@
-//
-// Created by albin on 29/08/2019.
-//
-
 #include "BUCKET.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+BUCKET::BUCKET() {}
+
+void BUCKET::sortingArray(int *arr, int n){
+    int max = 0;
+    for(int i=0; i<n; i++){
+        k = *(arr+1);
+        if(max < k){
+            max = k;
+        }
+    }
+
+    const int buckets = 10;
+
+    vector<float> b[buckets];
+
+    int devider = ceil((max +1)/buckets);
+
+    for (int i=0; i<n; i++)
+    {
+        k = *(arr+1);
+        int bi = floor(k / devider);
+        b[bi].push_back(k);
+    }
+
+    //Insertion sort in each bucket
+    for (int i =0; i<buckets; i++){
+        sort(b[i].begin(), b[i].end());
+    }
+
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < b[i].size(); j++)
+            arr[index++] = b[i][j];
+}
+
+void BUCKET::printArray(int *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+}
