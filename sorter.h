8566a0d1294f99f2a2e94a022254c20cd12206e2
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-09-02 18:25:08 -0500

    Update sorter.h

--- sorter.h
+++ sorter.h
@@ -8,7 +8,7 @@ class sorter
 	public:
 		static void insertionSortarr(int arr[], int n);
 		static void insertionSortVect(std::vector<int> &arr);
-		static void bucketSortarr(int arr[], int n);
+		static void bucketSortarr(double arr[], int n);
 		static void bucketSortVect(std::vector<int> &arr);
 };
 
